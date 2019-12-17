aff2984881ad47f1bfb632a9a805659fce4ccb40
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2019-12-17 15:33:50 -0600

    gitsecond

---
+++ sqldependency/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlDependency
+{
+    public class Class1
+    {
+    }
+}
